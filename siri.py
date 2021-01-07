4f68af34003796a69cdf3d2018085e3ec4631f8c
shivam <shivamkuma4jmp@gmail.com>
2021-01-07 21:34:31 +0530

    First Commit!

---
+++ siri.py
@@ -0,0 +1,66 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+
+listner = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    # engine.say('What can I do for you')
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listner.listen(source)
+            command = listner.recognize_google(voice)
+            command = command.lower()
+            if 'siri' in command:
+                command = command.replace('siri', '')
+                print(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        # print(time)
+        talk('Right now time is ' + time)
+    elif 'who is' in command:
+        person =  command.replace('who is' , '')
+        info = wikipedia.summary(person , 2)
+        print(info)
+        talk(info)
+    elif 'date' or 'girlfriend' in command:
+        talk('sorry , I have a Boyfriend')
+    elif 'are you single' in command:
+        talk('no, I am in a relationship with Internet')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    # elif 'rishika' in command:
+    #     talk('Rishika is very innocent and cute girl. born in munger on 29 october 1998. Rishika boyfriend is shivam kumar who is little bit mental')
+    else:
+        talk('I can not understand please repeat again')
+
+
+
+
+while True:
+    run_alexa()
